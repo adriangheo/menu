3f1004bc49f87dcdd8e47a87ecc5ed7915bb3e2b
Adrian Gheorghe <adrian.gh1989@gmail.com>
2022-08-28 15:30:46 +0300

    initial exercise setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import React, { useState } from 'react';
+import Menu from './Menu';
+import Categories from './Categories';
+import items from './data';
+
 function App() {
-  return (
-    <div className="App">
-      Hello React!
-    </div>
-  );
+  return <h2>menu project setup</h2>;
 }
 
 export default App;
